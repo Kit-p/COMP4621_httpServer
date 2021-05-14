e5ef390142953c05742977e257de84fbf5f836ab
PANG, Kit <kpangaa@connect.ust.hk>
2021-05-14 19:13:05 +0800

    Basic HTTP request parsing

--- server.cpp
+++ server.cpp
@@ -10,8 +10,20 @@
 // Global Constants
 const int SERVER_PORT = 80; // use port 80 for HTTP
 const int LISTENNQ = 5;
-const int MAXLINE = 100;
+const int MAXLINE = 8192;
 
+enum class HttpMethods
+{
+    UNDEFINED,
+    GET,
+    POST
+};
+
+class HttpRequest;
+
+bool startsWith(std::string, std::string);
+bool endsWith(std::string, std::string);
+HttpRequest *parse_request(int conn_fd);
 void request_handler(int conn_fd);
 
 int main()
@@ -65,4 +77,131 @@ int main()
 
 void request_handler(int conn_fd)
 {
+    HttpRequest *request = parse_request(conn_fd);
 }
+
+HttpRequest *parse_request(int conn_fd)
+{
+    int buffer_size = 0;
+    char buf[MAXLINE] = {0};
+    std::string msg = "";
+    HttpRequest *request = nullptr;
+
+    while (true)
+    {
+        buffer_size = recv(conn_fd, buf, MAXLINE - 1, 0);
+
+        if (buffer_size <= 0)
+        {
+            std::cerr << "Recv failed!" << std::endl;
+            continue;
+        }
+
+        if (buf[buffer_size] != '\0')
+        {
+            buf[buffer_size] = '\0';
+        }
+        msg += buf;
+
+        request = HttpRequest::parse(msg);
+        if (!request->isBad())
+        {
+            break;
+        }
+        delete request;
+        request = nullptr;
+    }
+
+    return request;
+}
+
+bool startsWith(std::string base, std::string compare)
+{
+    if (compare.length() <= 0 || base.length() < compare.length())
+    {
+        return false;
+    }
+    return base.compare(0, compare.length(), compare) == 0;
+}
+
+bool endsWith(std::string base, std::string compare)
+{
+    if (compare.length() <= 0 || base.length() < compare.length())
+    {
+        return false;
+    }
+    return base.compare(base.length() - compare.length(), std::string::npos, compare) == 0;
+}
+
+class HttpRequest
+{
+public:
+    HttpRequest() : method(HttpMethods::UNDEFINED), url(""), version("") {}
+
+    HttpMethods method;
+    std::string url;
+    std::string version;
+
+    bool isBad()
+    {
+        if (this->method == HttpMethods::UNDEFINED)
+            return true;
+
+        if (!startsWith(this->url, "/"))
+            return true;
+
+        if (!startsWith(this->version, "HTTP/"))
+            return true;
+
+        return false;
+    }
+
+    static HttpRequest *parse(std::string msg)
+    {
+        HttpRequest *request = new HttpRequest();
+
+        // parse
+        const std::string sp = " ";
+        const std::string crlf = "\r\n";
+
+        int start_pos = 0;
+        int end_pos = msg.find(sp);
+        if (start_pos >= msg.length() || end_pos == std::string::npos)
+        {
+            return request;
+        }
+
+        request->method = toMethod(msg.substr(start_pos, end_pos - start_pos));
+
+        start_pos = end_pos + 1;
+        end_pos = msg.find(sp);
+        if (start_pos >= msg.length() || end_pos == std::string::npos)
+        {
+            return request;
+        }
+
+        request->url = msg.substr(start_pos, end_pos - start_pos);
+
+        start_pos = end_pos + 1;
+        end_pos = msg.find(crlf);
+        if (start_pos >= msg.length() || end_pos == std::string::npos)
+        {
+            return request;
+        }
+
+        request->version = msg.substr(start_pos, end_pos - start_pos);
+
+        return request;
+    }
+
+    static HttpMethods toMethod(std::string method)
+    {
+        if (method == "GET" || method == "get")
+            return HttpMethods::GET;
+
+        if (method == "POST" || method == "post")
+            return HttpMethods::POST;
+
+        return HttpMethods::UNDEFINED;
+    }
+};
